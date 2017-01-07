153f917ee6b01575d08d136520a9c434740fa131
Gmaniacuk1 <gtrevellick@googlemail.com>
2017-01-07 12:08:50 +0000

    .

--- OpenInApp/source.extension.cs
+++ OpenInApp/source.extension.cs
@@ -11,7 +11,7 @@ namespace OpenInApp
         public const string Name = "Open in Paint.NET";
         public const string Description = @"Open a file in Paint.NET directly from Solution Explorer within Visual Studio.";
         public const string Language = "en-US";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Author = "GregTrevellick";
         public const string Tags = "open, open in, Paint.NET, Paint, PaintDotNet";
     }
